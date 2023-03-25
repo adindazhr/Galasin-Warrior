fd1196c27422b08061840ff8f36a2a4733e73208
adindazhr <adindazahra1920@gmail.com>
2023-03-25 12:58:44 +0700

    Second commit

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -1,23 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.AI;
+using Unity.MLAgents;
+using Unity.MLAgents.Sensors;
+using Unity.MLAgents.Actuators;
 
-public class Enemy : MonoBehaviour
+public class Enemy : Agent
 {
-    private Transform playerTransform;
-    private NavMeshAgent nav;
-    // Start is called before the first frame update
-    void Start()
-    {
-        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
-        nav = GetComponent<NavMeshAgent>();
-        
+    Rigidbody rBody;
+    void Start () {
+        rBody = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    //public Transform Penjaga;
+    public Transform Penyerang1;
+    public override void OnEpisodeBegin()
     {
-        nav.destination = playerTransform.position;
+        this.rBody.angularVelocity = Vector3.zero;
+        this.rBody.velocity = Vector3.zero;
+        this.transform.localPosition = new Vector3(Random.value,
+                                           2.317779f,
+                                           0.0262009f);
+
+        Penyerang1.localPosition = new Vector3( 27.00822f, 0.08f, -2.9266f);
+    }
+
+    public float forceMultiplier = 10;
+    public override void OnActionReceived(ActionBuffers actionBuffers)
+    {
+        // Actions, size = 2
+        Vector3 controlSignal = Vector3.zero;
+        controlSignal.x = actionBuffers.ContinuousActions[0];
+        controlSignal.z = actionBuffers.ContinuousActions[1];
+        rBody.AddForce(controlSignal * forceMultiplier);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player") == true)
+        {
+            SetReward(1.0f);
+            EndEpisode();
+        }
     }
 }
