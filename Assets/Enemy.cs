b708c0f039c39350d37f59ae334e05b16dd8cc29
adindazhr <adindazahra1920@gmail.com>
2023-03-25 12:56:10 +0700

    Second Commit

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -7,15 +7,40 @@ using Unity.MLAgents.Actuators;
 
 public class Enemy : Agent
 {
-    public float MoveSpeed=20;
-    private Vector3 orig; //original position
-    private Bounds bndFloor; //kalo jatoh
-    private GameObject Target;
-
-    public override Initialize()
-    {
-        orig = new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z);
-
+    Rigidbody rBody;
+    void Start () {
+        rBody = GetComponent<Rigidbody>();
     }
 
+    //public Transform Penjaga;
+    public Transform Penyerang1;
+    public override void OnEpisodeBegin()
+    {
+        this.rBody.angularVelocity = Vector3.zero;
+        this.rBody.velocity = Vector3.zero;
+        this.transform.localPosition = new Vector3(Random.value,
+                                           2.317779f,
+                                           0.0262009f);
+
+        Penyerang1.localPosition = new Vector3( 27.00822f, 0.08f, -2.9266f);
+    }
+
+    public float forceMultiplier = 10;
+    public override void OnActionReceived(ActionBuffers actionBuffers)
+    {
+        // Actions, size = 2
+        Vector3 controlSignal = Vector3.zero;
+        controlSignal.x = actionBuffers.ContinuousActions[0];
+        controlSignal.z = actionBuffers.ContinuousActions[1];
+        rBody.AddForce(controlSignal * forceMultiplier);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player") == true)
+        {
+            SetReward(1.0f);
+            EndEpisode();
+        }
+    }
 }
